e0e2980dadb48a2f9922789733e6e0a923aaf682
Asus <tarikhasan12200@gmail.com>
2022-02-27 00:27:45 +0600

    Adding Dynamic UI on Tap

--- Assets/Scripts/DragToMove.cs
+++ Assets/Scripts/DragToMove.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class DragToMove : MonoBehaviour
 {
     private Touch touch;
     private float speedModifier;
+    [SerializeField] private GameObject canvasComponent;
+    [SerializeField] private Button btn = null;
+
 
     // Start is called before the first frame update
     void Start()
@@ -16,8 +20,16 @@ public class DragToMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        btn.onClick.AddListener(NoParamaterOnclick);
+        //print(canvasComponent.activeSelf);
         if (Input.touchCount > 0)
         {
+            if (Input.GetTouch(0).tapCount == 2)
+            {
+                canvasComponent.SetActive(true);
+                print("double tap");
+            }
+
             touch = Input.GetTouch(0);
 
             if (touch.phase == TouchPhase.Moved)
@@ -27,4 +39,13 @@ public class DragToMove : MonoBehaviour
             }
         }
     }
+    private void NoParamaterOnclick()
+    {
+        if (canvasComponent.activeSelf == true)
+        {
+            canvasComponent.SetActive(false);
+        }
+    }
+
 }
+
